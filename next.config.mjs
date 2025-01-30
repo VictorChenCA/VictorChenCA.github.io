c844656599333a54f57a5466a29546614a17e673
Victor Chen <89617516+VictorChenCA@users.noreply.github.com>
2025-01-30 11:17:16 -0800

    Test 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,15 @@
-let userConfig = undefined
+let userConfig = undefined;
 try {
-    userConfig = await import('./v0-user-next.config')
+    userConfig = await import("./v0-user-next.config");
 } catch (e) {
     // ignore error
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
     output: "export",
-
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
     basePath: "/VictorChenCA.github.io",
-
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
+    assetPrefix: "/VictorChenCA.github.io",
     images: {
         unoptimized: true,
     },
@@ -37,36 +19,32 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    images: {
-        unoptimized: true,
-    },
     experimental: {
         webpackBuildWorker: true,
         parallelServerBuildTraces: true,
         parallelServerCompiles: true,
     },
-}
-
-mergeConfig(nextConfig, userConfig)
+};
 
 function mergeConfig(nextConfig, userConfig) {
     if (!userConfig) {
-        return
+        return nextConfig;
     }
 
     for (const key in userConfig) {
         if (
-            typeof nextConfig[key] === 'object' &&
+            typeof nextConfig[key] === "object" &&
             !Array.isArray(nextConfig[key])
         ) {
             nextConfig[key] = {
                 ...nextConfig[key],
                 ...userConfig[key],
-            }
+            };
         } else {
-            nextConfig[key] = userConfig[key]
+            nextConfig[key] = userConfig[key];
         }
     }
+    return nextConfig;
 }
 
-export default nextConfig
+export default mergeConfig(nextConfig, userConfig);
