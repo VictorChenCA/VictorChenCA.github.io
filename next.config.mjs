fa03c9da4c4f66152967e40e82df4af49b2a02ee
Victor Chen <89617516+VictorChenCA@users.noreply.github.com>
2025-02-05 14:04:08 -0800

    Test 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 const nextConfig = {
     output: "export",  // Required for GitHub Pages
-    basePath: "",  // Set to an empty string to avoid incorrect subdirectory issues
-    assetPrefix: "/",  // Ensures relative asset paths
+    basePath: "my-site",  // Set to an empty string to avoid incorrect subdirectory issues
+    assetPrefix: "/my-site/",  // Ensures relative asset paths
     trailingSlash: true,  // Helps with proper routing
     images: {
         unoptimized: true,  // Required for static export
